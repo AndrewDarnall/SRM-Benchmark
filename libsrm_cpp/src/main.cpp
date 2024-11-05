88dbddf1a4db04ae818ffda3f1baf73fbf27ec87
TheComputerScientist <DRNNRW00M10C351S@studium.unict.it>
2024-11-06 00:08:15 +0100

    chore(main): Tweaked the main.cpp

--- libsrm_cpp/src/main.cpp
+++ libsrm_cpp/src/main.cpp
@@ -9,17 +9,27 @@
 
 int main(int argc, char** argv) {
 
-    if (argc != 3) {
+    if (argc != 5) {
         std::cerr << "usage:\t" << argv[0] << "\t<PATH-TO-IMAGE>\t" <<
-        "<Q-Parameter>" << std::endl;
-        exit(-1);
+        "<Q-Parameter--Default=32>\t<Max-Regions--Default=15>\t" << 
+        "<Min-Size--Default=0.001>" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    // Open image and check for non-empty image
-    cv::Mat img = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
+    cv::Mat img = cv::imread(argv[1]);
+
+    if (img.empty()) {
+        std::cerr << " !! Error Opening the Image !! " << std::endl;
+        return EXIT_FAILURE;
+    }
 
     std::cout << " -- Running the Statistical Region " <<
     "Merging Algorithm on " << argv[1] << std::endl;
+
+    // Algorithm methods ...
+
+    std::cout << " == Finished Running the" << 
+    " SRM Algorithm == " << std::endl;
 
     return EXIT_SUCCESS;
 }
